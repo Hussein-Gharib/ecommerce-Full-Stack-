f060095827dfae9d2ebd6c8a0569cf73761a4468
Hussein-Gharib <gharibhu56@gmail.com>
2025-12-14 14:28:57 +0100

    cart setup

---
+++ src/routes/cartRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+const cartController = require('../controllers/cartController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+
+router.get('/', authMiddleware, cartController.getCart);
+
+
+router.post('/', authMiddleware, cartController.addToCart);
+
+
+router.put('/:itemId', authMiddleware, cartController.updateCartItem);
+
+
+router.delete('/:itemId', authMiddleware, cartController.removeCartItem);
+
+
+router.delete('/', authMiddleware, cartController.clearCart);
+
+module.exports = router;
